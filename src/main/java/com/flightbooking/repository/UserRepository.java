cf0c07385459098bcf95ffe772df1a1231ff0256
Shreya-glitch1 <shreyakalgaon2005@gmail.com>
2026-09-05 11:33:53 +0530

    chore: implemented registration and login feature

--- src/main/java/com/flightbooking/repository/UserRepository.java
+++ src/main/java/com/flightbooking/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.flightbooking.repository;
 import com.flightbooking.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
+@Repository
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
